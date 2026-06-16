a0aa09eefd5616163fc0b145b441f61cf3d524a9
azad-technext <azadashraf@technext.it>
2026-06-16 14:18:38 +0600

    add debug log

--- app/lib/turnstile.ts
+++ app/lib/turnstile.ts
@@ -10,3 +10,9 @@
  */
 export const TURNSTILE_SITE_KEY =
   process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY || "1x00000000000000000000AA";
+
+console.log(
+  process.env.NEXT_PUBLIC_TURNSTILE_SITE_KEY,
+  TURNSTILE_SITE_KEY,
+  "azad",
+);
